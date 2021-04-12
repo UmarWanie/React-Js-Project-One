ba7feba3faec016608ab554181e608ba6734fa0f
UmarWanie <73072928+UmarWanie@users.noreply.github.com>
2021-04-12 15:37:12 +0530

    Added Footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,10 +1,19 @@
-import React from "react"
+import React from "react";
 
 class Footer extends React.Component {
-  render() {
-    return (
-      <h1>Hello</h1>
-      )
-  }
+	render() {
+		return (
+			<footer class="footer">
+				<div class="container-fluid">
+					<nav>
+						<p class="copyright text-center">
+							©<script>document.write(new Date().getFullYear())</script>
+							Designed and Developed by <strong>UmarWani and Amir Sofi</strong>
+						</p>
+					</nav>
+				</div>
+			</footer>
+		);
+	}
 }
-export default Footer
+export default Footer;
